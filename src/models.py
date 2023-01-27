7950f3a2fc738f109dc2f9ba8dfcbef4ad731a14
DGgithubDG <thenumerousrandom@gmail.com>
2023-01-27 19:40:59 +0000

    done

--- src/models.py
+++ src/models.py
@@ -8,31 +8,69 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Follower(Base):
+    __tablename__ = 'follower'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    follower_from_id = Column(Integer, ForeignKey("user.id"))
+    follower_to_id = Column(Integer, ForeignKey("user.id"))
 
-    def to_dict(self):
-        return {}
+#   #  planet_climate = relationship(favoritePlanet)
+    follower = relationship(User)
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+class Post(Base):
+    __tablename__ = 'post'
+# #     # Here we define columns for the table address.
+# #     # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+
+    post = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey("user.id"))
+    post_id = Column(Integer, ForeignKey("comment.id"))
+
+#    #  ForeignKey("planets.id,user.id")
+    author = relationship(User)
+    post = relationship(Post)
+
+
+
+
+class Media(Base):
+    __tablename__ = 'media'
+# #     # Here we define columns for the table address.
+# #     # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey("post.id"))
+
+    post = relationship(Post)
+
+
+def to_dict(self):
+    return {}
+
+
+# Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
